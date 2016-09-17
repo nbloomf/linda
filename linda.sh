62f70e377e17eddbdcba49e29c20dd3c16bf2685
Nathan Bloomfield <nbloomf@gmail.com>
2016-09-16 20:29:29 -0500

    add words

--- linda.sh
+++ linda.sh
@@ -1,45 +1,7 @@
 #!/bin/bash
 
-cat $1 | \
-  egrep -v -f ~/code/linda/dict/nouns.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/ion-or.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/ation-er.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/e!ion-er.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/e!ion-or.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/e!ation-er.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/e!ation-or.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/y!ication-icand-ier.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/y!ication-icant-ier.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/ment-r.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-verbs/ment-er.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/ity-ist-ism.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/e!ity-ist-ism.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/ic!e-icist-icism.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/ous!ity.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/ous!osity.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/t!cy.txt | \
-  egrep -v -f ~/code/linda/dict/nouns/from-adjectives/t!ce.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-ed-ing.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-ped-ping.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-ted-ting.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-ned-ning.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-ged-ging.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/s-d-ing.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/es-ed-ing.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/e-es-ed-ing.txt | \
-  egrep -v -f ~/code/linda/dict/regular-verbs/y-ies-ied-ying.txt | \
-  egrep -v -f ~/code/linda/dict/irregular-verbs.txt | \
-  egrep -v -f ~/code/linda/dict/adjectives.txt | \
-  egrep -v -f ~/code/linda/dict/determiners.txt | \
-  egrep -v -f ~/code/linda/dict/colors.txt | \
-  egrep -v -f ~/code/linda/dict/adverbs.txt | \
-  egrep -v -f ~/code/linda/dict/cardinals.txt | \
-  egrep -v -f ~/code/linda/dict/conjunctions.txt | \
-  egrep -v -f ~/code/linda/dict/ordinals.txt | \
-  egrep -v -f ~/code/linda/dict/qualifiers.txt | \
-  egrep -v -f ~/code/linda/dict/pronouns.txt | \
-  egrep -v -f ~/code/linda/dict/prepositions.txt | \
-  egrep -v -f ~/code/linda/dict/auxiliary-verbs.txt | \
-  egrep -v -f ~/code/linda/dict/names.txt | \
-  cat
+find ./dict -type f -exec cat {} + > tmp.txt
+
+cat $1 | egrep -v -f ~/code/linda/tmp.txt
+
+rm tmp.txt
